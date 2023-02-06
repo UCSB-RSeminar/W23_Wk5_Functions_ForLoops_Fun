5aa3c8ba8dc0b72e3f04db847ffbfa341a507645
gaberunte <gabe.runte@gmail.com>
2023-02-06 12:38:38 -0800

    new plotting function

--- src/my_functions.R
+++ src/my_functions.R
@@ -33,3 +33,21 @@ read_tree_data = function(file){
 }
 
 
+
+sim_plot = function(x_vector,  pred, prey){
+  
+
+  colors = rep(NaN, 2)
+  
+  for(i in 1:2){
+    r.g.b = runif(1:3, min = 0, max = 1)
+    colors[i] <- rgb(r.g.b[1], r.g.b[2],r.g.b[3])
+    }
+  
+  
+  plot(x_vector, pred, type = "l", lwd= "3", col = colors[1], xlab = "Time", ylab = "Population sizes", ylim = c(0, 1.4*max(prey)))
+      lines(x_vector, prey, lwd = "3", col = colors[2])
+      legend(x_vector[0.75*length(x_vector)], 1.4*max(prey), legend = c("Prey", "Predator"),col=c(colors[2], colors[1]), cex = 1, lty = 1, lwd = 3)
+      
+  
+}
